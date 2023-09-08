8622ef2f2f979ffc1543dbe5db9ba1f6416a340b
silviapetrino <petrino.silvia@gmail.com>
2023-09-08 17:57:11 +0200

    [edit] js

--- js/script.js
+++ js/script.js
@@ -1,38 +1,46 @@
-// Descrizione:** Scrivere un programma che chieda all’utente:
-// Il numero di chilometri da percorrere
-// Età del passeggero Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
-// MILESTONE 2: Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l’output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-// Nota: Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
 
 
-const kilometres = document.getElementById("kilometres").value;
-const kmPrice = 0.21;
-const ticketPrice = kmPrice * kilometres;
-let discount = 0;
-let age = document.getElementById("age");
 const buttonOk = document.getElementById("button1");
-let finalPrice = (ticketPrice - discount).toFixed(2) + "€";
-const showTicket = document.getElementById("show-ticket"); 
 
 
 buttonOk.addEventListener("click", function(){
-  showTicket.className = "d-block";
 
-  if (age.value = "underage") {
-    discount = (ticketPrice * 20)/100;}
+  const inputKilometres = document.getElementById("kilometres");
+  const kmValue = inputKilometres.value;
+  const kmPrice = 0.21;
+  const ticketPrice = kmPrice * kmValue;
+  let discount = 0;
+  let finalPrice = (ticketPrice - discount).toFixed(2) + "€";
+
+  console.log(ticketPrice);
+
+  console.log(finalPrice);
+
+  const age = document.getElementById("age").value;
+  
+  console.log(age);
+
 
 })
 
 
 
-console.log(finalPrice)
 
 
 
+
+
+
+
+
+// const kilometres = document.getElementById("kilometres").value;
+// const kmPrice = 0.21;
+// const ticketPrice = kmPrice * kilometres;
+// let discount = 0;
+// let age = document.getElementById("age");
+// const buttonOk = document.getElementById("button1");
+// let finalPrice = (ticketPrice - discount).toFixed(2) + "€";
+// const showTicket = document.getElementById("show-ticket"); 
 
 
 
