f352d5b133f4c54f52dd41885d15b3c689a92cf9
silviapetrino <petrino.silvia@gmail.com>
2023-09-09 09:50:53 +0200

    [add] all edits

--- js/script.js
+++ js/script.js
@@ -1,27 +1,65 @@
 
 
+
 const buttonOk = document.getElementById("button1");
+const buttonHide = document.getElementById("button2")
+const showTicket = document.getElementById("show-ticket");
+
+buttonHide.addEventListener("click", function () {
+ showTicket.className =  " d-none";
+  
+})
 
 
 buttonOk.addEventListener("click", function(){
+  
+  const name = document.getElementById("name").value;
 
   const inputKilometres = document.getElementById("kilometres");
-  const kmValue = inputKilometres.value;
+  let kmValue = inputKilometres.value;
   const kmPrice = 0.21;
-  const ticketPrice = kmPrice * kmValue;
-  let discount = 0;
-  let finalPrice = (ticketPrice - discount).toFixed(2) + "€";
+  const ticketPrice = (kmPrice * kmValue).toFixed(2);
 
-  console.log(ticketPrice);
+  const age = document.getElementById("age");
 
-  console.log(finalPrice);
-
-  const age = document.getElementById("age").value;
+  let message = "";
+  let finalPrice 
   
-  console.log(age);
+
+  if (age.value == "underage") {
+    const discountYoung = ((ticketPrice*20)/100).toFixed(2);
+    message = "In quanto minorenne, pagherai solo "
+    finalPrice = (ticketPrice - discountYoung).toFixed(2);
+
+  } else if (age.value == "elder") {
+    const discountElder = ((ticketPrice*40)/100).toFixed(2);
+    console.log(discountElder);
+    message = "In quanto Over65, pagherai solo "
+    finalPrice = (ticketPrice - discountElder).toFixed(2);
+
+  } else if (age.value == "adult") {
+    finalPrice = ticketPrice;
+    message = "In quanto maggiorenne, la tua tariffa è "
+  }
 
 
+let justPay = document.getElementById("message-pr");
+  justPay.innerHTML = message + finalPrice + "€";
+  justPay.classList.add("color-txt-sp");
+
+
+document.getElementById("pr-ticket").innerHTML = finalPrice + "€";
+
+
+document.getElementById("name-pass").innerHTML = name.toUpperCase();
+
+document.getElementById("barcode").innerHTML = 
+Math.floor(Math.random(1) * 100000000);            
 })
+
+         
+
+
 
 
 
